95ebb6ebdc1bd2e938be5c395e84d85f9a8487d9
vinicius <vinicius>
2023-01-18 06:56:41 -0300

    vini0

--- src/component/Navibar.jsx
+++ src/component/Navibar.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './css/Navibar.css';
 
 const Navibar = () => {
-  return (<div></div>), (<h1>Navbar</h1>);
+  return (
+  <div></div>),
+  (<h1>Navbar.1</h1>
+  );
 };
 
 export default Navibar;
